f9b0318727dfcb69d2f9e50bbdeb43a949d5e111
Jesus Brancacho <jesus.brancacho@pucp.edu.pe>
2025-12-05 17:45:44 -0500

    Update contarAnomalias.cpp

--- DivideYVenceras/contarAnomalias.cpp
+++ DivideYVenceras/contarAnomalias.cpp
@@ -3,34 +3,45 @@ using namespace std;
 
 int ContarCentro(int *arr,int ini,int medio,int fin) {
 
+    //definimos un tamaño que sea del tamaño del otro arreglo
     int size = fin-ini+1;
     int temp[size];
 
-    //copiar a temp
+    //copiar a temporal
     for (int i=0;i<size;i++) {
         temp[i]=arr[ini+i];
     }
+
+
+    //indice relativo de la mitad izquierda
     int izquierda = 0;
+
+    //indice relativo de la mitad derecha
     int derecha = medio - ini + 1;
     int contador= 0;
 
-    for (int indice=0;indice<size;indice++) {
+    
+    for (int j=0;j<size;j++) {
         if (derecha<= fin - ini) {
             if (izquierda <= medio-ini) {
                 if (temp[izquierda] > temp[derecha]) {
 
                     contador+=1;
-                    arr[ini+indice]=temp[derecha++];
+                    arr[ini+j]=temp[derecha++];
                 }
                 else {
-                    arr[ini+indice]=temp[izquierda++];
+                    arr[ini+j]=temp[izquierda++];
                 }
+            }
+            else{
+            
+                arr[ini+j] = temp[derecha++];
             }
 
         }
         else {
             //solo quedan elementos en izquierda
-            arr[ini+indice]=temp[izquierda++];
+            arr[ini+j]=temp[izquierda++];
         }
 
     }
@@ -41,14 +52,25 @@ int ContarCentro(int *arr,int ini,int medio,int fin) {
 
 int Contador(int* arr,int ini,int fin) {
     int medio = (ini+fin)/2;
-
+    
    if (ini>=fin) return 0;
+    //PARES ANOMALOS  (3,4)(3,5) (4,5)
+    //BUSCAMOS EN LA PRIMERA MITAD 
+    //MEDIO = 2
 
-    int izquierda = Contador(arr,ini,medio);
-    int derecha =  Contador(arr,medio+1,fin);
-    int centro = ContarCentro(arr,ini,medio,fin);
 
-    return izquierda+derecha+centro;
+    //  0  1  2
+    // [10,12,14]
+   int izquierda = Contador(arr,ini,medio);
+   
+   //   3, 4  ,5   //SOLO 3,4
+   // [13, 11, 15]
+   int derecha =  Contador(arr,medio+1,fin);
+   
+   //ahora buscamos en el centro
+   int centro = ContarCentro(arr,ini,medio,fin);
+
+   return izquierda+derecha+centro;
 
 
 
@@ -60,7 +82,10 @@ int main() {
                  //0  1  2  3  4  5
     int entrada[]={10,12,14,13,11,15};
     int n= 6;
+    //queremos contar pares anomales arr[i]> arr[j] 
     cout<<"El numero es: "<<Contador(entrada,0,n-1)<<endl;
+
+    
     return 0;
 }
 
